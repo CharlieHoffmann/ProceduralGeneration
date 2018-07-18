abad5275da29f007f277d761d0080b5052775065
Charlie Hoffmann <34170461+CharlieHoffmann@users.noreply.github.com>
2018-07-18 09:55:23 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ var flying = 0;
 var terrain = [];
 
 function setup() {
+    // set width and height to that of the browser window
     createCanvas(windowWidth, windowHeight, WEBGL);
   cols = w / scl;
   rows = h/ scl;
@@ -27,17 +28,19 @@ function draw() {
   for (var y = 0; y < rows; y++) {
     var xoff = 0;
     for (var x = 0; x < cols; x++) {
+        //set sizes of perlin noise scaling
       terrain[x][y] = map(noise(xoff, yoff), 0, 1, -120, 120);
       xoff += 0.2;
     }
     yoff += 0.2;
   }
 
-
+  //change colors and camera position
   background(0);
   translate(0, -100);
-    stroke(102, 153, 255);
+  stroke(102, 153, 255);
 
+    //create the drawing
   rotateX(PI/3);
   noFill();
   translate(-w/2, -h/2+50);
